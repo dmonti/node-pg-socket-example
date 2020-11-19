c09e373f82f6957eb48c720ad233a4aa406be84b
Daniel Monti <dms.monti@gmail.com>
2020-11-18 22:34:03 -0300

    New dependencies endpoint

--- src/controllers/application.controller.js
+++ src/controllers/application.controller.js
@@ -3,3 +3,7 @@ const package = require("../../package")
 exports.getInfo = async function (req, res, next) {
     return res.status(200).json({ name: package.name, version: package.version, status: 'UP' })
 }
+
+exports.getDependencies = async function (req, res, next) {
+    return res.status(200).json({ dependencies: package.dependencies })
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,6 @@ const router = express.Router()
 
 var AppController = require('../controllers/application.controller')
 router.get('/info', AppController.getInfo)
+router.get('/dependencies', AppController.getDependencies)
 
 module.exports = router
